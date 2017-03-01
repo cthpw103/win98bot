64391b12a7ebffebd0cd88a0ed19759c242ad03d
Charles Milette <charles.milette@gmail.com>
2017-03-01 17:15:09 -0500

    Changed the pile of if statements for a switch statement

--- bot.js
+++ bot.js
@@ -6,49 +6,43 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === '#minecraftsucks') {
-    msg.reply('FUCKKK YOOU!!!! MINECRAFT IS THE BEST GAME!!!!  YOOU JUST PLAY GAY ASS MARIO!  AND GAY ASS ZELDA!')
-    };
-  if (msg.content === 'rd') {
-    msg.reply('Can you just fuck off? Actually learn a programming language.')
-    };
-  if (msg.content === '#invite') {
-    msg.reply('silly you this is a selfbot')
-    };
-  if (msg.content === '#hack') {
-    msg.reply('Trolled. You cannot hack discord you skid unless you are a faggot.')
-    };
-  if (msg.content === '#help') {
-    msg.reply('`#minecraftsucks | Get a SMK-type response.` `rd | Cancer.` `#invite | Gives an invite to the bot.` `#hack | Runs a dank exploit that gives you ownership.` `#avatar | Gets your avatar.`')
-    };
-// kill me now
-  if (msg.content === 'memz') {
-    msg.reply('https://www.youtube.com/watch?v=2k0SmqbBIpQ')
-    };
-// god help us
-  if (msg.content === 'hunter') {
-    msg.reply('Hunter is dead.  LLOL NVM HE BACK LOL HE SUCK MORE JULIA PUSSY! HE WILL SHOVE MEMZ UP HER BUTTHOLE AGAIN!')
-    };
-// recursion
-  if (msg.content === 'recursive tag') {
-    msg.channel.sendMessage('.tag recursive')
-    };
-// lol
-  if (msg.content === 'doom') {
-    msg.reply('https://www.youtube.com/watch?v=QLKZhu_dgxA')
-    };
-// gooatse
-  if (msg.content === '#goatse') {
-    msg.reply('http://goatse.info/hello.jpg')
-    };
-// yee
-  if (msg.content === '#yee') {
-    msg.reply('https://www.youtube.com/watch?v=q6EoRBvdVPQ')
-    };
-// this should do it
-  if (msg.content === '#avatar') {
-    msg.reply(msg.author.avatarURL)
-  };
+  switch (msg.content) {
+    case '#minecraftsucks':
+      msg.reply('FUCKKK YOOU!!!! MINECRAFT IS THE BEST GAME!!!!  YOOU JUST PLAY GAY ASS MARIO!  AND GAY ASS ZELDA!');
+      break;
+    case 'rd':
+      msg.reply('Can you just fuck off? Actually learn a programming language.');
+      break;
+    case '#invite':
+      msg.reply('silly you this is a selfbot');
+      break;
+    case '#hack':
+      msg.reply('Trolled. You cannot hack discord you skid unless you are a faggot.');
+      break;
+    case '#help':
+      msg.reply('`#minecraftsucks | Get a SMK-type response.` `rd | Cancer.` `#invite | Gives an invite to the bot.` `#hack | Runs a dank exploit that gives you ownership.` `#avatar | Gets your avatar.`');
+      break;
+    case 'memz': // kill me now
+      msg.reply('https://www.youtube.com/watch?v=2k0SmqbBIpQ');
+      break;
+    case 'hunter': // god help us
+      msg.reply('Hunter is dead.  LLOL NVM HE BACK LOL HE SUCK MORE JULIA PUSSY! HE WILL SHOVE MEMZ UP HER BUTTHOLE AGAIN!');
+      break;
+    case 'recursive tag': // recursion
+      msg.channel.sendMessage('.tag recursive');
+      break;
+    case 'doom': // lol
+      msg.reply('https://www.youtube.com/watch?v=QLKZhu_dgxA');
+      break;
+    case '#goatse': // gooatse
+      msg.reply('http://goatse.info/hello.jpg');
+      break;
+    case '#yee': // yee
+      msg.reply('https://www.youtube.com/watch?v=q6EoRBvdVPQ');
+      break;
+    case '#avatar': // this should do it
+      msg.reply(msg.author.avatarURL);
+      break;
 });
 
 client.login('token goes here');
